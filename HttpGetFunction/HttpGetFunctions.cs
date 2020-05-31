efdcb1a4082ac890d29709e66d54b9ca242e8c27
Cory Koehler <cjiek9181@gmail.com>
2020-05-31 13:09:59 -0400

    Add publish profile and azcli scripts

--- HttpGetFunction/HttpGetFunctions.cs
+++ HttpGetFunction/HttpGetFunctions.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Web.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.VisualBasic;
 
 namespace HttpGetFunction
 {
